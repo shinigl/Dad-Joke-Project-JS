575fa99990a2990bdf4c5d5d20d26ac78436cf17
shinigl <kraniket754@gmail.com>
2024-11-08 14:29:58 +0530

    Changes in API

--- script.js
+++ script.js
@@ -5,29 +5,34 @@ const shareFacebook = document.getElementById('share-facebook');
 const shareWhatsApp = document.getElementById('share-whatsapp');
 
 
-let API = `https://official-joke-api.appspot.com/random_joke`
-async function fetchJoke(){
 
-    try{
-    let response = await fetch(API); //fetch jokes from API server
-    let data = await response.json();
-    console.log(data);
-    const jokeText = data.setup + " " + data.punchline;
-    joke.innerText = jokeText;
-    
+async function fetchJoke() {
+    let API = 'https://api.api-ninjas.com/v1/dadjokes'
+
+    let header = {
+        headers: { 'X-Api-Key': '9raQ+d2yDMHXn0i+c13/0A==sw0YqFrpPoVF8w5B' }
     }
-    catch(error){
-    console.log(error);
+
+    try {
+        let response = await fetch(API,header); //fetch jokes from API server
+        let data = await response.json();
+        console.log(data);
+        const jokeText = `${data[0].joke}🤣`;
+        joke.innerText = jokeText;
+
     }
-   
-}   
+    catch (error) {
+        console.log(error);
+    }
+
+}
 
 // Function to update share links
 function updateShareLinks(jokeText) {
     // Encode the joke text for URL use
     const encodedJokeText = encodeURIComponent(jokeText);
 
-  
+
     shareTwitter.href = `https://twitter.com/intent/tweet?text=${encodedJokeText}`;
 
     shareFacebook.href = `https://www.facebook.com/sharer/sharer.php?u=${encodedJokeText}`;
